97cee6046d2aedf33c0dd067648b778cdc8588cd
SHAMARBible <SHAMARBible@gmail.com>
2026-03-29 08:59:44 -0500

    Added GitHub actions workflow

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Add this
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Add this
-  ],
+export default defineConfig(({ command }) => {
+  return {
+    // If we are deploying via Github Actions without a custom domain, 
+    // we need to set the base to the repository name for GitHub Pages to resolve assets correctly.
+    base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+    plugins: [
+      react(),
+      tailwindcss(), // Add this
+    ],
+  }
 })
